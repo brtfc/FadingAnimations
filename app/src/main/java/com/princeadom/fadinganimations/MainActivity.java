0a0ffaddf5e2b45956814720e3fdd41ec2ce167f
Prince Adom <adomprince@gmail.com>
2019-03-13 03:51:12 -0400

    added an if else structure and boolean expressions to revert to the original image

--- app/src/main/java/com/princeadom/fadinganimations/MainActivity.java
+++ app/src/main/java/com/princeadom/fadinganimations/MainActivity.java
@@ -7,31 +7,31 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    boolean ironmanIsShowing = true;
 
     public void fadeImage(View view){
         ImageView ultron_imgVw = findViewById(R.id.ultron);
         //image view for ironman
         ImageView ironman_imgVw = findViewById(R.id.ironman);
-        boolean ironmanIsShowing = true;
-
-        if(ironmanIsShowing){
-            //fade out the ultron imageview
-            ultron_imgVw.animate().alpha(0).setDuration(30000);
-            //fade in the ironman imageview simultaneously
-            ironman_imgVw.animate().alpha(1).setDuration(30000);
-            ironmanIsShowing = false;
-        }
-        else {
-            //fade out the ultron imageview
-            ultron_imgVw.animate().alpha(0).setDuration(30000);
-            //fade in the ironman imageview simultaneously
-            ironman_imgVw.animate().alpha(1).setDuration(30000);
-            ironmanIsShowing = true;
-        }
 
         //setDuration() takes as arguments the number of seconds but measured in milliseconds
         // 1 second = 1000 millisecond
         //alpha() takes a value between 0 and 1
+
+        if(ironmanIsShowing){
+            //fade out the ultron imageview
+            ultron_imgVw.animate().alpha(0).setDuration(2000);
+            //fade in the ironman imageview simultaneously
+            ironman_imgVw.animate().alpha(1).setDuration(2000);
+            ironmanIsShowing = false;
+        }
+        else {
+            //fade out the ultron imageview
+            ultron_imgVw.animate().alpha(1).setDuration(2000);
+            //fade in the ironman imageview simultaneously
+            ironman_imgVw.animate().alpha(0).setDuration(2000);
+            ironmanIsShowing = true;
+        }
 
 
 
